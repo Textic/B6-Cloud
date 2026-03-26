5e0349b44bb7baea081ed92c8fe9ab6f24195cd2
textic <danielbienchido@gmail.com>
2026-03-25 22:09:48 -0300

    feat: Se conecta exitosamente y logra desplegar en azure

--- desplegar-usuarios.cmd
+++ desplegar-usuarios.cmd
@@ -1,11 +1,22 @@
 @echo off
-echo Iniciando despliegue de Funcion de Usuarios a Azure...
+set "APP_NAME=B6FuncionTex"
+set "RG_NAME=B6Grupo"
+
+echo [1/4] Seleccionando suscripcion Azure...
+call az account set --subscription f620bf86-f52f-4e36-861f-d1d94b342485
+
+echo [2/4] Compilando y Preparando archivos locales...
 cd faas-usuarios
-call mvn clean package azure-functions:deploy
-if %errorlevel% neq 0 (
-    echo Error en el despliegue de Usuarios.
-    pause
-    exit /b %errorlevel%
-)
-echo Despliegue de Usuarios completado con exito.
+call mvn clean package -DskipTests
+if %errorlevel% neq 0 (echo Error en compilacion && pause && exit /b 1)
+
+echo [3/4] Comprimiendo archivos para Azure...
+powershell -Command "Compress-Archive -Path target/azure-functions/%APP_NAME%/* -DestinationPath target/azure-functions/%APP_NAME%/%APP_NAME%.zip -Force"
+if %errorlevel% neq 0 (echo Error al comprimir && pause && exit /b 1)
+
+echo [4/4] Subiendo codigo a %APP_NAME% en Azure...
+call az functionapp deployment source config-zip -g %RG_NAME% -n %APP_NAME% --src target/azure-functions/%APP_NAME%/%APP_NAME%.zip
+if %errorlevel% neq 0 (echo Error en el despliegue CLI && pause && exit /b 1)
+
+echo DESPLIEGUE DE USUARIOS COMPLETADO CON EXITO
 pause
